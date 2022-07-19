e70674d5f8ce9f6d350aa499b76ca773ce0e5d3d
ArturBatista <artur.batista07@gmail.com>
2022-07-18 22:47:07 -0300

    estrutura inicial

---
+++ src/main/java/br/com/senai/uc8projeto/Uc8ProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.uc8projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc8ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc8ProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/uc8projeto/Uc8ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.uc8projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc8ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
