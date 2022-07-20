6cbb6ab998bf3313fffb4b1266b78559df696d71
Eviner <eviner.lisboa@gmail.com>
2022-07-20 17:18:40 -0300

    Adicionado campo cpf_cnjp à entidade Usuario

--- src/main/java/br/com/senai/uc8projeto/model/Usuario.java
+++ src/main/java/br/com/senai/uc8projeto/model/Usuario.java
@@ -16,6 +16,7 @@ public class Usuario {
 	private String nome;
 	private String senha;
 	private String email;
+	private String cpf_cnpj;
 	
 	public int getId() {
 		return id;
@@ -43,5 +44,11 @@ public class Usuario {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getCpf_cnpj() {
+		return cpf_cnpj;
+	}
+	public void setCpf_cnpj(String cpf_cnpj) {
+		this.cpf_cnpj = cpf_cnpj;
 	}
 }
