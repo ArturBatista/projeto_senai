c9448ae2c0fb7db41122516a65920260d87200b8
mateuslph <mlptche@gmail.com>
2022-07-24 20:40:29 -0300

    recriado a entidade solicitante

---
+++ src/main/java/br/com/senai/uc8projeto/model/Solicitante.java
@@ -0,0 +1,52 @@
+package br.com.senai.uc8projeto.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Solicitante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private Localizacao localizacao;
+	private String telefone;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getCpf_cnpj() {
+		return cpf_cnpj;
+	}
+	public void setCpf_cnpj(String cpf_cnpj) {
+		this.cpf_cnpj = cpf_cnpj;
+	}
+	private String cpf_cnpj;
+
+}
